353ffecc287443550f676b55d1775a41cb82597a
Ak-Aryan005 <akaryankumar486@gmail.com>
2026-02-19 14:04:45 +0530

    Refactor multer storage configuration and path handling

--- backend/app/middleware/multer.ts
+++ backend/app/middleware/multer.ts
@@ -1,14 +1,20 @@
+import fs from "fs";
+import path from "path";
 import multer from "multer";
 
-const storage = multer.diskStorage({
-destination: function (req, file, cb) {
-cb(null, "./public/temp")
-},
-filename: function (req, file, cb) {
+const uploadPath = path.join(process.cwd(), "public", "temp");
 
-cb(null, file.originalname)
+if (!fs.existsSync(uploadPath)) {
+  fs.mkdirSync(uploadPath, { recursive: true });
 }
-})
-// export const upload = multer({ storage: storage })
-export const upload = multer({ storage }).array("media", 10); 
-// "media" is the key sent from frontend
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, uploadPath);
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+
+export const upload = multer({ storage }).array("media", 10);
